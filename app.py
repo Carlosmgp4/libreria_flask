dc6b1d3e11ed892593c30fbdf9dcee660846e52f
Carlosmgp4 <carlosmanuelgamezperezdeguzman@gmail.com>
2021-05-02 22:22:15 +0200

    He creado el html que mostrará la información de cada libro y he añadido al programa la forma dinamica de acceder a dicho html, de este modo con un solo html mostrara la información de cada libro cuando se le solicite el libro en concreto.

--- app.py
+++ app.py
@@ -9,4 +9,11 @@ with open ("books.json") as f:
 def inicio():
     return render_template("inicio.html", lista=datos)
 
+@app.route('/<isbn>')
+def detalles(isbn):
+    for lib in datos:
+        if lib["isbn"]==isbn:
+            return render_template("detalles.html",libro=lib)
+    abort(404)
+
 app.run("0.0.0.0",5000,debug=True)
